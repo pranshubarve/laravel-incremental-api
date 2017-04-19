0a4fcc5f75fdf9b4d9e8770a6bcf6fee98fa8922
Pranshu Barve <pranshubarve@gmail.com>
2017-04-19 08:50:03 +0530

    Categories Migration, Seeder, Controller and Models
     Create migrations seeder controller and models for categories and users. Also create API controller.

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 30) as $index)
+        {
+            Category::create([
+                'title' => $faker->sentence(5),
+                'description' => $faker->paragraph(4),
+            ]);
+        }
+    }
+}
